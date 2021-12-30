53171efbd5911859fc2e76c38e5931ea5259be02
Eviamar <eviamar26@gmail.com>
2021-12-30 22:21:33 +0200

    30122021_4

--- identify.js
+++ identify.js
@@ -2,10 +2,10 @@ function Check()
 {
     let username = document.getElementById("uname").value;
     let password = document.getElementById("pswd").value;
-    let users = ["Evyatar","Yulia","Igor"];
-    let passwords = ["12345","258456","159753"]
+    let users = ["Evyatar","Yulia","Igor","mel"];
+    let passwords = ["12345","258456","159753","mel"]
     flag = false;
-    for(i=0; i<3;i++)
+    for(i=0; i<4;i++)
     if(username == users[i] && password == passwords[i])
     {
         flag = true;
