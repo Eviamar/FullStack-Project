8a6c01206317dbdd212a5498df0dc0968168eace
Eviamar <eviamar26@gmail.com>
2022-01-01 18:10:15 +0200

    01012022_4

--- music.js
+++ music.js
@@ -2,22 +2,26 @@ function Music()
 {
     document.getElementById("stylesheet").href="musiccss.css";
     document.getElementById("dancer").hidden = false;
+    document.getElementById("secondnav").hidden = true;
     document.getElementById("dancer").src="pics/dancer.gif";
     document.getElementById("maintitle").innerHTML="<a href='https://youtu.be/rDBbaGCCIhk' target='_blank'>Miami Nights 1984 - Accelerated</a>";
     document.getElementById('playme').hidden = false;
     document.getElementById('playme').innerHTML="<marquee behavior='alternate' scrollamount='25'>Hope you are enjoying!!!</marquee>";
+                    /*29000*/ 
     setTimeout(Guitar,29000); 
 }
 function Guitar()
 {
     document.getElementById("guitar").hidden = false;
     document.getElementById("guitar").src="pics/rockoutguitar.gif";
+                            /*30000*/
     setTimeout(GuitarGoHome,30000);
 }
 
 function GuitarGoHome()
 {
     document.getElementById("guitar").hidden = true;
+                            /*59000*/ 
     setTimeout(GuitarComesBack,59000);
 }
 function GuitarComesBack()
@@ -28,6 +32,7 @@ function GuitarComesBack()
     document.getElementById("yulia").hidden = false;
     document.getElementById("igor").hidden = false;
     document.getElementById("guitar").hidden = false;
+                            /*59000*/ 
     setTimeout(PartyIsOver,59000);
 }
 
@@ -38,7 +43,7 @@ function PartyIsOver()
     document.getElementById("dancer").hidden = true;
     document.getElementById("yulia").hidden = true;
     document.getElementById("igor").hidden = true;
-    document.getElementById("sndnav").hidden = false;
+    document.getElementById("secondnav").hidden = false;
     document.getElementById("stylesheet").href="sitecss.css";
     document.getElementById("maintitle").innerHTML="Home";
     document.getElementById('playme').innerHTML="play me and wait for it!";
